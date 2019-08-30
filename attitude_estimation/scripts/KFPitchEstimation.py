c592d733d616b4a5825a5c34c03a5ab4af85e131
Sylvain <sylvain@email.address>
2019-08-30 16:39:11 +0200

    attitude estim: comments added to code and launch files modified

--- attitude_estimation/scripts/KFPitchEstimation.py
+++ attitude_estimation/scripts/KFPitchEstimation.py
@@ -15,8 +15,7 @@ import tf
 import numpy as np
 import math
 import KalmanFilter as kf
-#from dynamic_reconfigure.server import Server
-#from simulator.cfg import attitudeCtrlCFGConfig
+
 
 
 
@@ -36,6 +35,7 @@ pubGroundTruthRPY = rospy.Publisher('GroundTruthRPYAngles', RPYAngles, queue_siz
 Te = 1.0/25.0   # IMU Frequency: 25Hz
 IMURate = rospy.Rate(1./Te)
 
+
 # system definition
 # state : pitch angle (rad), gyro's bias (rad/s)
 # input: pitch rate (rad/s) from gyro measurement
@@ -48,7 +48,6 @@ Bk = np.array( [ [Te],[0.0] ] )
 Ck = np.array( [[1.0, 0.0]] )
     
 
-
 # filter instantiation
 pitchKF = kf.KalmanFilter(nx, nu, ny)
 pitchKF.setStateEquation(Ak, Bk)
@@ -57,15 +56,6 @@ pitchKF.setCk(Ck)
 
 
 ''' dynamic parameters '''
-'''
-# noises std devs
-gyroPitchRateStdDev = 0.0065 #0.0065*0.0065  # (rad/s)
-gyroPitchRateBiasStdDev = 0.00005  # (rad/s)
-acceleroPitchStdDev = 0.01  # (rad)
-# cov matrices
-Qk = Te * np.array( [ [math.pow(gyroPitchRateStdDev,2), 0.0], [0.0, math.pow(gyroPitchRateBiasStdDev,2)] ] )
-Rk = np.array([math.pow(acceleroPitchStdDev,2)])
-'''
 
 # ----------------------------------------------------------------------------
 def callbackDynParam(config, level):
@@ -74,21 +64,22 @@ def callbackDynParam(config, level):
     global gyroPitchRateStdDev, gyroPitchRateBiasStdDev, acceleroPitchStdDev
     global Qk, Rk, pitchKF
 
+    # standard deviations of noises
     gyroPitchRateStdDev = float("""{GyroPitchRateStdDev}""".format(**config))
     gyroPitchRateBiasStdDev = float("""{GyroPitchRateBiasStdDev}""".format(**config))
     acceleroPitchStdDev = float("""{AcceleroPitchStdDev}""".format(**config))
     
+    # state noise covariance matrix (rate gyro and bias)
     Qk = Te * np.array( [ [math.pow(gyroPitchRateStdDev,2), 0.0], [0.0, math.pow(gyroPitchRateBiasStdDev,2)] ] )
+    # measurement noise covariance matrix  (accelero)
     Rk = np.array([math.pow(acceleroPitchStdDev,2)])
     
     pitchKF.setQk(Qk)
     pitchKF.setRk(Rk)
     
    
-
     return config
 # -----------------------------------------------------------------------------
-
 
 # server for dyamic parameters
 srv = Server(KFPitchCFGConfig, callbackDynParam)
@@ -114,9 +105,7 @@ P0 = np.array( [ [0.5,0.0],[0.0,0.1] ] )
 pitchKF.initFilter( x0 , P0 )
 
  
-
-
-# measurements
+# data structures for measurements from IMU
 accMeas = np.zeros((3,1))
 gyroMeas = np.zeros((3,1))
 magMeas = np.zeros((3,1))
@@ -134,6 +123,7 @@ pitchGroundTruth = 0.
 rollGroundTruth = 0.
 quaternionGroundTruth = Quaternion()
 msgGroundTruthRPY = RPYAngles()
+
 
 
 # subscribers callbacks
@@ -162,6 +152,7 @@ def callBackImuAcceleroGyro(data):
     quaternionGroundTruth.z = data.orientation.z
     quaternionGroundTruth.w = data.orientation.w
     
+    # convert quaternion to Euler angles roll, pitch, yaw
     anglesGroundTruth = tf.transformations.euler_from_quaternion([quaternionGroundTruth.x, quaternionGroundTruth.y, quaternionGroundTruth.z, quaternionGroundTruth.w], axes='sxyz')
     rollGroundTruth = anglesGroundTruth[0]
     pitchGroundTruth = anglesGroundTruth[1]
@@ -169,23 +160,23 @@ def callBackImuAcceleroGyro(data):
     
     #print((rollGroundTruth, pitchGroundTruth, yawGroundTruth))
     
-    # read pitch rate measurement from gyro         
+    # read angular velocity measurement for pitch from gyro         
     uk = gyroMeas[1,0]
+    # KF prediction step
     pitchKF.predict(uk)
     
     # compute pitch measurement from accelero    
     yk = math.atan2(-accMeas[0,0] , math.sqrt( math.pow(accMeas[1,0],2) + math.pow(accMeas[2,0],2) ) )
+    # KF update step
     pitchKF.update(yk)
 
+    # get pitch estimate
     pitchEstim = pitchKF.xk[0,0]
-    
-    
-      
     
 # -----------------------------------------------------------------------------        
 
 
-
+## ****** uncomment to extend the code to yaw estimation *********
 ## -----------------------------------------------------------------------------
 #def callBackImuMagneto(data):
 ## -----------------------------------------------------------------------------
@@ -201,6 +192,8 @@ def callBackImuAcceleroGyro(data):
 # subscribers
 # ------------
 rospy.Subscriber("/imu/data", Imu, callBackImuAcceleroGyro)
+
+## ****** uncomment to extend the code to yaw estimation *********
 #rospy.Subscriber("/imu/mag", MagneticField, callBackImuMagneto)
 
 
@@ -216,21 +209,27 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         
+        # get current time
         timeNow = rospy.Time.now()
     
+        # message to be published with estimates of roll, pitch and yaw 
         msgEstimatedRPY.header.seq = msgEstimatedRPY.header.seq + 1
         msgEstimatedRPY.header.stamp = timeNow
-        msgEstimatedRPY.roll = np.nan # not a number (not computed)
+        msgEstimatedRPY.roll = np.nan # not a number (not computed -> to be completed to extend the code)
         msgEstimatedRPY.pitch = pitchEstim
-        msgEstimatedRPY.yaw =  np.nan # not computed (not computed)
+        msgEstimatedRPY.yaw =  np.nan # not a computed (not computed -> to be completed to extend the code)
+        # publish message        
         pubEstimatedRPY.publish(msgEstimatedRPY)
     
+        # message to be published with ground truth values of roll, pitch and yaw 
         msgGroundTruthRPY.header.seq = msgGroundTruthRPY.header.seq + 1
         msgGroundTruthRPY.header.stamp = timeNow
         msgGroundTruthRPY.roll = rollGroundTruth
         msgGroundTruthRPY.pitch =  pitchGroundTruth
         msgGroundTruthRPY.yaw = yawGroundTruth
+        # publish message        
         pubGroundTruthRPY.publish(msgGroundTruthRPY)  
         
+        # sleep during one sampling period
         IMURate.sleep()
 # -----------------------------------------------------------------------------
